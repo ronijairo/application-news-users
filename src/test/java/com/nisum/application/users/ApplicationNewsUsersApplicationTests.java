fcd18d505851d8623128b3c11c8a59ed733f519d
ronijairo <roni.jcanche@gmail.com>
2021-10-25 16:48:33 -0500

    commit inicial

---
+++ src/test/java/com/nisum/application/users/ApplicationNewsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.application.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationNewsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
